c8c2cba6f32af251b7ce23284c5d6883c5ed2780
AimanFred <irzanfarish@gmail.com>
2024-10-10 12:30:42 +0800

    change message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Express on Vercel");
+    res.send("Boleh ni deploy Kashaf");
 });
 
 const PORT = process.env.PORT || 5000;
